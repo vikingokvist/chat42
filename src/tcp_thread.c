4954afbb9a67749f920dbfc724a6ff63bb7a1400
vikingokvist <vikingokvist@gmail.com>
2025-11-06 16:34:19 +0100

    new change

--- src/tcp_thread.c
+++ src/tcp_thread.c
@@ -1,5 +1,27 @@
 #include "../inc/chat42.h"
 
+
+char *strjoin(const char *s1, const char *s2)
+{
+	size_t len1, len2;
+	char *res;
+
+	if (!s1 || !s2)
+		return (NULL);
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	res = malloc(len1 + len2 + 1);
+	if (!res)
+		return (NULL);
+
+	memcpy(res, s1, len1);
+	memcpy(res + len1, s2, len2);
+	res[len1 + len2] = '\0';
+	return (res);
+}
+
 int tcp_struct_init(t_tcp  *tcp, t_client **users_table) {
 
     tcp->OWN_USERNAME = get_user_name();
@@ -88,7 +110,14 @@ void send_tcp_message(t_client *client, const char *msg)
 		close(client_sockfd);
 		return ;
 	}
-
-	write(client_sockfd, msg, strlen(msg));
+    char *new_msg = strjoin(client->MACHINE_ID, "::");
+    char *tmp = strjoin(new_msg, client->USERNAME);
+    free(new_msg);
+    char *tmp2 = strjoin(tmp, ": ");
+    free(tmp);
+    char *tmp3 = strjoin(tmp2, msg);
+    free(tmp2);
+	write(client_sockfd, tmp3, strlen(tmp3));
 	close(client_sockfd);
+    free(tmp3);
 }
